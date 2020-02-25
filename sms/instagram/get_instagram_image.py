46f1716cb9f3d697efa373dc16125d7a5d285992
Chenghao Zhu <zhuchcn@gmail.com>
2020-02-24 17:39:07 -0800

    add prefix index

--- sms/instagram/get_instagram_image.py
+++ sms/instagram/get_instagram_image.py
@@ -2,6 +2,7 @@ import requests
 import re
 import argparse
 import os
+import math
 from pathos.multiprocessing import ProcessingPool as Pool
 
 
@@ -10,30 +11,47 @@ class InstagramImageCrawler():
     input_path = None
     output_dir = None
     nthreads = 1
+    prefix_index = False
     verbose = False
 
-    def __init__(self, input_path, output_path, nthreads, verbose):
+    def __init__(self, input_path, output_path, nthreads, verbose, prefix_index):
         self.input_path = input_path
         self.output_dir = output_path
         self.nthreads = nthreads
         self.verbose = verbose
+        self.prefix_index = prefix_index
         self.crawl_images()
     
     def crawl_images(self):
+        if self.prefix_index:
+            line_count = 0
+            with open(self.input_path, "rt") as fh:
+                for l in fh:
+                    line_count += 1
+            index_width = int(math.log10(line_count)) + 1
         with open(self.input_path, "rt") as fh:
+            i = 1
             if self.nthreads == 1:
                 for line in fh:
-                    self.get_image(line.rstrip())
+                    prefix = format(i, f"0{index_width}d") + "_" \
+                                    if self.prefix_index else None
+                    self.get_image(line.rstrip(), prefix)
+                    i += 1
             else:
                 urls = []
+                prefixs = []
                 for line in fh:
                     urls.append(line.rstrip())
+                    prefixs.append(format(i, f"0{index_width}d") + "_" \
+                                   if self.prefix_index else None)
+                    i += 1
                     if len(urls) == self.nthreads:
-                        Pool().map(self.get_image, urls)
+                        Pool().map(self.get_image, urls, prefixs)
                         urls = []
-                Pool().map(self.get_image, urls)
+                        prefixs = []
+                Pool().map(self.get_image, urls, prefixs)
 
-    def get_image(self, url):
+    def get_image(self, url, prefix):
         res = requests.get(f"{self.baseurl}{url}")
         if res.status_code != 200:
             if self.verbose:
@@ -46,7 +64,10 @@ class InstagramImageCrawler():
             return
         
         post_id = re.sub("^https://www.instagram.com/p/(.+)/", "\\1", url)
-        output_file = os.path.join(self.output_dir, post_id + ".jpg")
+        output_file = post_id + ".jpg"
+        if prefix:
+            output_file = prefix + output_file
+        output_file = os.path.join(self.output_dir, output_file)
         with open(output_file, "wb") as fh:
             fh.write(res.content)
         if self.verbose:
@@ -57,6 +78,7 @@ def main():
     parser.add_argument('-i', '--input-path', type=str)
     parser.add_argument('-o', '--output-path', type=str)
     parser.add_argument('-n', "--nthreads", type=int, default=1)
+    parser.add_argument('-p', '--prefix-index', action="store_true")
     parser.add_argument('-v', '--verbose', action="store_true")
     args = parser.parse_args()
 
@@ -67,7 +89,7 @@ def main():
         raise argparse.ArgumentTypeError("-n/--nthreads must be larger than 0.")
 
     InstagramImageCrawler(args.input_path, args.output_path, args.nthreads, 
-                          args.verbose)
+                          args.verbose, args.prefix_index)
 
 if __name__ == "__main__":
     main()
