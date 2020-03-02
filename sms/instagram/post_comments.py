13ae9fc0317bd538e5db30a2cdf19dcd1c689a82
Chenghao Zhu <zhuchcn@gmail.com>
2020-03-02 10:07:50 -0800

    fix bug, that the browser is closed every 20 seconds

--- sms/instagram/post_comments.py
+++ sms/instagram/post_comments.py
@@ -10,6 +10,21 @@ import re
 _PAGE_UNAVAILABLE_ERROR = "Sorry, this page isn't available."
 _PAGE_CANT_OPEN_ERROR = "Page can't be opened."
 
+# In the current version of pyppeteer, the browser is closed for every ~20 
+# seconds. This bug isn't fixed yet. The following work around is provided by:
+# https://github.com/pyppeteer/pyppeteer2/issues/6
+def disable_timeout_pyppeteer():
+    import pyppeteer.connection
+    original_method = pyppeteer.connection.websockets.client.connect
+    def new_method(*args, **kwargs):
+        kwargs['ping_interval'] = None
+        kwargs['ping_timeout'] = None
+        return original_method(*args, **kwargs)
+
+    pyppeteer.connection.websockets.client.connect = new_method
+
+disable_timeout_pyppeteer()
+
 class InstagramPostComments():
     browser = None
     page = None
